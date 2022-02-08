aeadc32aa008047ada699bca934be66a162a7231
Brandon Dombrowsky <brandondombrowsky@gmail.com>
2022-02-08 09:24:43 -0800

    .button {   background-color: #4CAF50; /* Green */   border: none;   color: white;   padding: 15px 32px;   text-align: center;   text-decoration: none;   display: inline-block;   font-size: 16px; }

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Topbar from './components/Topbar/Topbar'
 import CardNav from './components/CardNav/CardNav';
 import Flashcard from './components/Flashcard/Flashcard';
+import Button from './components/Button/Button';
+
+
+const controls = ['Back', 'Flip', 'Next']
 
 function App() {
   return (
@@ -10,7 +14,14 @@ function App() {
       <Topbar />
       <div className='container'>
         <CardNav />
+        <div className='card-container'>
         <Flashcard />
+          <div className='card-controls'>
+            {controls.map((control) => {
+              return <Button className='button'>{control}</Button>
+            })}
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
--- client/src/components/CardNav/CardNav.js
+++ client/src/components/CardNav/CardNav.js
@@ -1,18 +1,14 @@
 import React from "react";
 import './CardNav.css'
 
+const cardLinks = ['Card A', 'Card B', 'Card C', 'Card D', 'Card E', 'Card F', 'Card G', 'Card H']
 function CardNav() {
     return (
         <div className='card-nav'>
             <ul>
-                <li>Card A</li>
-                <li>Card B</li>
-                <li>Card C</li>
-                <li>Card D</li>
-                <li>Card E</li>
-                <li>Card F</li>
-                <li>Card G</li>
-                <li>Card H</li>
+               {cardLinks.map((link) => {
+                   return (<li>{link}</li>)
+               })}
             </ul>
         </div>
     )
